154aad30d061a8d68d91d9d8197fd93a8b12b975
lucorrear <lucorrear@unal.edu.co>
2024-04-23 16:32:25 -0500

    lab 3

--- pregunta.py
+++ pregunta.py
@@ -13,9 +13,18 @@ import pandas as pd
 
 
 def ingest_data():
-
-    #
-    # Inserte su código aquí
-    #
-
+    file_path = 'clusters_report.txt'
+    data = []
+    with open(file_path, 'r') as file:
+        lines = file.readlines()[2:]
+        for line in lines:
+            if line[0].isdigit():
+                columns = line.split()
+                cluster = int(columns[0])
+                cantidad_palabras = int(columns[1])
+                porcentaje_palabras = float(columns[3].strip('%'))
+                palabras_clave = ', '.join(columns[4:]).replace(',', ', ')
+                data.append([cluster, cantidad_palabras, porcentaje_palabras, palabras_clave])
+    df = pd.DataFrame(data, columns=['cluster', 'cantidad_de_palabras', 'porcentaje_de_palabras', 'palabras_clave'])
     return df
+df = ingest_data()
