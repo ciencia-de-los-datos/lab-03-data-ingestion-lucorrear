03a081027a8190505626e6b2829768b80d5ccbc1
lucorrear <lucorrear@unal.edu.co>
2024-04-24 09:31:51 -0500

    Lab 3

--- pregunta.py
+++ pregunta.py
@@ -10,33 +10,17 @@ espacio entre palabra y palabra.
 
 """
 import pandas as pd
+import re
 
 def ingest_data():
-    # Definir el nombre del archivo
-    file_path = 'clusters_report.txt'
-
-    # Lista para almacenar los datos procesados
-    data = []
-
-    # Leer el archivo y procesar los datos
-    with open(file_path, 'r') as file:
-        # Saltar las primeras dos líneas y la línea del separador de guiones
-        next(file)
-        next(file)
-        next(file)
-        for line in file:
-            # Dividir la línea en columnas
-            columns = line.split(maxsplit=3)
-            # Obtener los datos de las columnas
-            cluster = columns[0]
-            cantidad_palabras = columns[1]
-            porcentaje_palabras = columns[2].replace(',', '.').strip('%')
-            palabras_clave = ', '.join(columns[3].split())
-            # Agregar los datos procesados a la lista
-            data.append([cluster, cantidad_palabras, porcentaje_palabras, palabras_clave])
-
-    # Crear el DataFrame de Pandas
-    df = pd.DataFrame(data, columns=['cluster', 'cantidad_de_palabras', 'porcentaje_de_palabras', 'palabras_clave'])
-
-    # Devolver el DataFrame
+    df = pd.read_fwf('clusters_report.txt', 
+                    colspecs="infer", 
+                    widths=[9, 16, 16, 80], 
+                    header=None, names=["cluster", "cantidad_de_palabras_clave", "porcentaje_de_palabras_clave", "principales_palabras_clave"],
+                    converters={"porcentaje_de_palabras_clave": lambda x: x.rstrip(" %").replace(",", ".")}
+                    ).drop(index={0,1,2}).ffill()   
+    df.columns = df.columns.str.lower()
+    df.columns = df.columns.str.replace('', '_')
     return df
+df = ingest_data()
+print(df)
